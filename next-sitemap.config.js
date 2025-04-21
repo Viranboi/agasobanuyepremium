328ca82730ba25070523027a2dea839805db4fb1
viranboi <168511383+Viranboi@users.noreply.github.com>
2025-04-21 06:12:20 +0000

    Added sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://agasobanuye.pro', // ✅ Your live website domain
+    generateRobotsTxt: true, // ✅ Also generate robots.txt
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
