c8e7b9b7dd51826b87ac703d3ec84030079625ad
viranboi <168511383+Viranboi@users.noreply.github.com>
2025-04-21 23:38:45 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const config = {
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
-   * @see https://github.com/vercel/next.js/issues/41980
+   * 
    */
   i18n: {
     locales: ["en"],
